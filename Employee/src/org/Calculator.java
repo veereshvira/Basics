3131611c1071de970dc1b5167d9806840d606984
veereshvira <110618907+veereshvira@users.noreply.github.com>
2024-02-14 11:26:55 +0530

    Add files via upload

---
+++ Employee/src/org/Calculator.java
@@ -0,0 +1,10 @@
+package org;
+
+public class Calculator {
+	public void add(int x, int y) {
+		System.out.println("suum of "+x+" is " +y+" is "+(x+y));
+	}
+	public void mul(int x, int y) {
+		System.out.println("suum of "+x+" is " +y+" is "+(x*y));
+	}
+}
---
+++ Employee/src/org/Emp.java
@@ -0,0 +1,7 @@
+package org;
+
+public class Emp {
+	public void display(int id,String name) {
+		System.out.println("Emp id "+name+" is "+id);
+	}
+}
