89ad55af29035376216acd882a62ea61e65a5bb6
veereshvira <110618907+veereshvira@users.noreply.github.com>
2024-02-14 11:27:24 +0530

    Add files via upload

---
+++ Encapsulation/src/com/jspider/p1/Demo2.java
@@ -0,0 +1,16 @@
+package com.jspider.p1;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		{
+			Demo1 d1 = new Demo1();
+			d1.display();
+//			System.out.println("var2 value is"+d1.var2);
+//			System.out.println("var3 value is"+d1.var3);
+//			System.out.println("var4 value is"+d1.var4);
+		}
+
+	}
+
+}
---
+++ Encapsulation/src/com/jspider/p2/Sample.java
@@ -0,0 +1,12 @@
+package com.jspider.p2;
+import com.jspider.p1.Demo1;
+public class Sample {
+	void display() 
+	{
+	Demo1 d1 = new Demo1();
+//			
+//	System.out.println("var2 value is"+d1.var2);
+//	System.out.println("var3 value is"+d1.var3);
+	System.out.println("var4 value is"+d1.var4);
+}
+}
